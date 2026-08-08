3f61fe8a6acd2a08a66a674534ede7666f465de5
AlphaGames30 <edonis.imbert@gmail.com>
2026-08-08 18:32:52 +0200

    Add __manifest__.py for Cache-Cache GPS module

---
+++ __manifest__.py
@@ -0,0 +1,12 @@
+{
+    'name': 'Cache-Cache GPS',
+    'version': '1.0',
+    'category': 'Website',
+    'summary': 'Jeu de cache-cache GPS en temps réel',
+    'depends': ['base', 'website', 'bus'],
+    'data': [
+        'views/game_template.xml',
+    ],
+    'installable': True,
+    'application': True,
+}
